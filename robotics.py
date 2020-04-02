f6c1f9350c7eb78df13f025fd64a81f6f05a46f2
olaals <olaalstad96@gmail.com>
2020-04-02 22:03:08 +0200

    finished 2b ex5

--- robotics.py
+++ robotics.py
@@ -99,3 +99,62 @@ def rotation_matrix_from_vectors(vec1, vec2):
     rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
     return rotation_matrix
 
+def exponentialRotToRot(axis, theta):
+    w_skew = skew(axis)
+    R = np.eye(3) + np.sin(theta)*w_skew + (1-np.cos(theta))*w_skew@w_skew
+    return R
+
+def exponentialRotToRot2(u):
+    theta = np.linalg.norm(u)
+    axis = u/theta
+    return exponentialRotToRot(axis, theta)
+
+def MatrixLog3(R):
+    """Computes the matrix logarithm of a rotation matrix
+    :param R: A 3x3 rotation matrix
+    :return: The matrix logarithm of R
+    Example Input:
+        R = np.array([[0, 0, 1],
+                      [1, 0, 0],
+                      [0, 1, 0]])
+    Output:
+        np.array([[          0, -1.20919958,  1.20919958],
+                  [ 1.20919958,           0, -1.20919958],
+                  [-1.20919958,  1.20919958,           0]])
+    """
+    acosinput = (np.trace(R) - 1) / 2.0
+    if acosinput >= 1:
+        return np.zeros((3, 3))
+    elif acosinput <= -1:
+        if not NearZero(1 + R[2][2]):
+            omg = (1.0 / np.sqrt(2 * (1 + R[2][2]))) \
+                  * np.array([R[0][2], R[1][2], 1 + R[2][2]])
+        elif not NearZero(1 + R[1][1]):
+            omg = (1.0 / np.sqrt(2 * (1 + R[1][1]))) \
+                  * np.array([R[0][1], 1 + R[1][1], R[2][1]])
+        else:
+            omg = (1.0 / np.sqrt(2 * (1 + R[0][0]))) \
+                  * np.array([1 + R[0][0], R[1][0], R[2][0]])
+        return VecToso3(np.pi * omg)
+    else:
+        theta = np.arccos(acosinput)
+        return theta / 2.0 / np.sin(theta) * (R - np.array(R).T)
+
+def so3ToVec(so3mat):
+    """Converts an so(3) representation to a 3-vector
+    :param so3mat: A 3x3 skew-symmetric matrix
+    :return: The 3-vector corresponding to so3mat
+    Example Input:
+        so3mat = np.array([[ 0, -3,  2],
+                           [ 3,  0, -1],
+                           [-2,  1,  0]])
+    Output:
+        np.array([1, 2, 3])
+    """
+    return np.array([so3mat[2][1], so3mat[0][2], so3mat[1][0]])
+
+def matrixLog3AngAx(R):
+    R_log = MatrixLog3(R)
+    angAx = so3ToVec(R_log)
+    return angAx
+
--- test.py
+++ test.py
@@ -1,22 +1,9 @@
-import robotics as rb
-import visualize as viz
-import vision as vsn
-import matplotlib.pyplot as plt
 import numpy as np
+import robotics as rb
+import math
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-
-viz.defaultAxisSetup(ax)
-
-
-p1 = np.array([2.0, 4.0, 2.0, 1.0])
-p2 = np.array([2.0, 1.0, 2.0, 1.0])
-p4 = np.array([2.0, 4.0, 1.0, 1.0])
-p3 = np.array([2.0, 1.0, 1.0, 1.0])
-
-
-viz.plotPlaneFrom4Points(ax, p1, p2, p3, p4)
-
-plt.show()
+R = rb.rotx(math.pi/8)
+print(R)
+print(math.pi/8)
+R_log = rb.matrixLog3AngAx(R)
+print(R_log)
